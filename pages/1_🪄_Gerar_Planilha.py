a7e07130528173cbe1841f3d45b9ef5a1a2c3a96
Bernardo Kuppens <gab360k@gmail.com>
2023-12-04 16:46:49 -0300

    conversão DateTime v2

--- pages/1_🪄_Gerar_Planilha.py
+++ pages/1_🪄_Gerar_Planilha.py
@@ -53,7 +53,12 @@ elif len(files) == 2 and (type_val):
     )
     df = processa(ref, txt)
     with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
-        # Write each dataframe to a different worksheet.
+        # Converte colunas DA- para formato de data
+        for col in df.columns:
+            if col.startswith('DA-'):
+                df[col] = pd.to_datetime(df[col], errors='coerce')
+
+        # Escreve para arquivo Excel
         df.to_excel(writer, sheet_name="PRINCIPAL", index=False)
 
         workbook  = writer.book
@@ -66,7 +71,7 @@ elif len(files) == 2 and (type_val):
                 worksheet.set_column(col_num, col_num, None, date_format)
 
         writer.close()
-        
+
         st.write("Concluído! 🪄")
         download = st.download_button(
             label="Fazer Download da Planilha",
