80b379653f1c9e63ef51b6f2bf3c7597cb611e54
Bernardo Kuppens <gab360k@gmail.com>
2023-12-04 18:05:54 -0300

    UX redirect, balloons, restore DateTime to original

--- pages/1_🪄_Gerar_Planilha.py
+++ pages/1_🪄_Gerar_Planilha.py
@@ -1,23 +1,33 @@
 import streamlit as st
+import pyautogui
 from core import unzip, processa
 import pandas as pd
 import time
 import io
 
 st.set_page_config(page_title="Gerar Planilha", page_icon="🪄", layout="wide")
-st.title("Gerar Planilha 🪄")
-st.subheader("Faça upload dos arquivos do Extrator, e baixe uma planilha formatada")
-st.info(
-    "Atenção: faça upload dos arquivos REF e TXT, ambos no formato .gz original (zipados)"
-)
+
+# Creating a dictionary of placeholders
+placeholders = {
+    "title": st.empty(),
+    "instruction": st.empty(),
+    "alert": st.empty(),
+    "alert2": st.empty(),
+    "file_uploader": st.empty()
+}
+
+# Using the placeholders
+placeholders["title"].title("Gerar Planilha 🪄")
+placeholders["instruction"].write("Faça upload dos arquivos do Extrator, e baixe uma planilha formatada")
+placeholders["alert"].info("Atenção: faça upload dos arquivos REF e TXT, ambos no formato .gz original (zipados)")
 
 # Initialize Variables
+buffer = io.BytesIO()
 has_ref, has_txt = False, False
 if "file_uploader_key" not in st.session_state:
     st.session_state["file_uploader_key"] = 0
-buffer = io.BytesIO()
 
-files = st.file_uploader(
+files = placeholders["file_uploader"].file_uploader(
     accept_multiple_files=True,
     label="Adicione os arquivos .gz",
     type=["gz"],
@@ -38,49 +48,44 @@ for i, f in enumerate(files):
 
 type_val = has_ref == True and has_txt == True
 
-if len(files) == 2 and type_val == False:
-    st.write(
-        "Faça Upload apenas dos arquivos originais do Extrator (formato REF.GZ e TXT.GZ)"
-    )
+if files and len(files) != 2:
+    placeholders["alert2"].warning("Faça Upload dos DOIS arquivos originais do Extrator (formato REF.GZ e TXT.GZ)")
     time.sleep(5)
     st.session_state["file_uploader_key"] += 1
     st.experimental_rerun()
 
 elif len(files) == 2 and (type_val):
-    st.write("Iniciando Processamento 🔨")
-    ref, txt = unzip(
-        files=files, ref_file_index=ref_file_index, txt_file_index=txt_file_index
-    )
-    df = processa(ref, txt)
-    with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
-        # Converte colunas DA- para formato de data
-
-        # Escreve para arquivo Excel
-        df.to_excel(writer, sheet_name="PRINCIPAL", index=False)
-
-        for col in df.columns:
-            if col.startswith('DA-'):
-                df[col] = pd.to_datetime(df[col], errors='coerce')
-
-        workbook  = writer.book
-        worksheet = writer.sheets['PRINCIPAL']
-        date_format = workbook.add_format({'num_format': 'dd/mm/yyyy'})
-
-        for col_num, value in enumerate(df.columns.values):
-            if value.startswith('DA-'):
-                # Apply the format to the column. Excel columns start from 0
-                worksheet.set_column(col_num, col_num, None, date_format)
-
-        writer.close()
-
-        st.write("Concluído! 🪄")
-        download = st.download_button(
-            label="Fazer Download da Planilha",
-            data=buffer,
-            file_name=f"{output_num}_tratado.xlsx",
-            mime="application/vnd.ms-excel",
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        pass
+    with col2:
+        ref, txt = unzip(
+            files=files, ref_file_index=ref_file_index, txt_file_index=txt_file_index
         )
-        if download:
-            st.cache_data.clear()
-            st.session_state["uploaded_files"] = False
-            files = 0
+        df = processa(ref, txt)
+        with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
+            df.to_excel(writer, sheet_name="PRINCIPAL", index=False)
+            writer.close()
+            # Emptying previous placeholders
+            for placeholder in placeholders.values():
+                placeholder.empty()
+            st.title("Feshow!")
+            download = st.download_button(
+                label="Fazer Download da Planilha",
+                data=buffer,
+                file_name=f"{output_num}_tratado.xlsx",
+                mime="application/vnd.ms-excel",
+            )
+            st.balloons()
+            if download:
+                st.cache_data.clear()
+                st.session_state["uploaded_files"] = False
+                files = 0
+
+            another = st.button(label="Gerar Outra Planilha")
+
+            if another:
+                pyautogui.hotkey("ctrl","F5")
+                
+    with col3:
+        pass
