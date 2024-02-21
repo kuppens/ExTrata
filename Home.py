5936248197ba05a7aa5a4d12ffcaf63631bfd9c3
Bernardo Kuppens <gab360k@gmail.com>
2024-02-21 14:18:07 -0300

    visual context

--- Home.py
+++ Home.py
@@ -1,15 +1,16 @@
 import streamlit as st
 
-st.set_page_config(page_title="ExTrata", page_icon="🍅", layout="wide")
+st.set_page_config(page_title="ExTrato", page_icon="🍅", layout="wide")
 
 
 co1, co2 = st.columns(2)
 
 with co1:
-    st.write("# ExTrata 🍅")
+    st.write("# ExTrato")
     st.write("### Ferramentas do Extrator")
 
     st.write('Navegue pelas Ferramentas pelo menu à esquerda!')
 
 with co2:
-    st.write("![Era para ter um GIF aqui...](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaG5sMXNwcGR2eW00M3RpeWF1ZDU4OXIyenZyNzhrNzF1aTg2bDJhNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o85xxpDp9J4Jlv62Q/giphy.gif)")
+    st.write("![Era para ter um GIF aqui...](https://media.tenor.com/F2PjYZeqkt8AAAAi/tomato.gif)")
+
