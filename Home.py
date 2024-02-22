4d26ad80208438856e12655d1dd37eb93bc5c195
Bernardo Kuppens <gab360k@gmail.com>
2024-02-22 15:50:36 -0300

    gif

--- Home.py
+++ Home.py
@@ -6,11 +6,11 @@ st.set_page_config(page_title="ExTrato", page_icon="🍅", layout="wide")
 co1, co2 = st.columns(2)
 
 with co1:
-    st.write("# ExTrato")
+    st.write("# ExTrato🍅")
     st.write("### Ferramentas do Extrator")
-
     st.write('Navegue pelas Ferramentas pelo menu à esquerda!')
 
 with co2:
-    st.write("![Era para ter um GIF aqui...](https://media.tenor.com/F2PjYZeqkt8AAAAi/tomato.gif)")
+    st.image("https://media.tenor.com/gO5PVeNebZIAAAAi/%D1%82%D0%BE%D0%BC%D0%B0%D1%82-%D0%B4%D0%B0.gif", width=300)
+
 
